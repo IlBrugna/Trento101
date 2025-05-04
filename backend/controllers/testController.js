4d5d15ba14a837ee4d076a25a07bb7466fa32a4c
IlBrugna <davidbrugnara15@gmail.com>
2025-05-04 20:57:46 +0200

    added route and controller to get sample from test collection

--- backend/controllers/testController.js
+++ backend/controllers/testController.js
@@ -1,3 +1,5 @@
+import testModel from "../models/testmodel.js"; // IMPORTA IL MODELLO
+
 export const home = (req,res)=>{
     const message = "this is the home route";
     res.send(message);
@@ -6,4 +8,13 @@ export const home = (req,res)=>{
 export const inner = (req,res)=>{
     const message = "this is the inner route";
     res.send(message);
+}
+
+export const database = async (req, res) => {
+    try {
+        const test = await testModel.find({}); // Trova tutti i documenti nel modello Test
+        res.status(200).json(test); // Invia i risultati come risposta JSON
+    } catch (error) {
+        res.status(500).json({ message: 'Errore durante il recupero dei dati dal database' });
+    }
 }
--- backend/routes/testRoute.js
+++ backend/routes/testRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'; //IMPORTA EXPRESS
-import { home, inner } from '../controllers/testController.js'; //IMPORTA LE FUNZIONI DAL CONTROLLER
+import { home, inner, database } from '../controllers/testController.js'; //IMPORTA LE FUNZIONI DAL CONTROLLER
 
 const router = express.Router(); //CREA ROUTER
 
 router.get('/', home );
 router.get('/inner', inner);
+router.get('/database', database);
 
 export default router; //ESPORTA IL ROUTER, DEFAULT COSì PUO AVERE NOME A SCELTA
