8dfe6e56d6c112b8b8807834c53f2ab7862e05ed
IlBrugna <davidbrugnara15@gmail.com>
2025-05-08 17:50:04 +0200

    adjusted cards

--- frontend/src/components/SectionCards.vue
+++ frontend/src/components/SectionCards.vue
@@ -5,15 +5,15 @@ import { ref } from 'vue'; //REF è WRAPPER PER VARIBILI, LE RENDE REATTIVE
 
 <template>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
-      <div class="bg-white shadow rounded-lg p-6 text-center">
+      <div class="bg-white shadow rounded-lg p-12 text-center flex flex-col justify-center min-h-[500px]">
         <h2 class="text-xl font-semibold mb-2">Servizi Comune</h2>
         <p class="text-gray-600">Scopri i servizi offerti dal comune.</p>
       </div>
-      <div class="bg-white shadow rounded-lg p-6 text-center">
+      <div class="bg-white shadow rounded-lg p-12 text-center flex flex-col justify-center min-h-[500px]">
         <h2 class="text-xl font-semibold mb-2">Aziende</h2>
         <p class="text-gray-600">Soluzioni per le imprese.</p>
       </div>
-      <div class="bg-white shadow rounded-lg p-6 text-center">
+      <div class="bg-white shadow rounded-lg p-12 text-center flex flex-col justify-center min-h-[500px]">  
         <h2 class="text-xl font-semibold mb-2">Servizi Università</h2>
         <p class="text-gray-600">Portale per studenti e docenti.</p>
       </div>
