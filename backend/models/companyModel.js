2665085b0ef889a50c1e639c70864ce0da1aadad
IlBrugna <davidbrugnara15@gmail.com>
2025-05-09 15:26:55 +0200

    added database model for companies

---
+++ backend/models/companyModel.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const companySchema = new mongoose.Schema({ //CREO SCHEMA COMPANY
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phoneNumber:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    address:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    website:{
+        type: String,
+        required: true,
+    },
+    picutre:{
+        type: String,
+        required: false,
+    }
+});
+
+const companyModel = mongoose.model('companies', companySchema); //CREO IL MODELLO DELLE COMPANY
+export default companyModel; 
