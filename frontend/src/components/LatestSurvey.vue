6a5492a8ea69affc969b58f0d2252450b8f1c687
markobeko <marko.markovic@studenti.unitn.it>
2025-05-09 12:59:15 +0200

    Placeholder sondaggi

--- frontend/src/components/LatestSurvey.vue
+++ frontend/src/components/LatestSurvey.vue
@@ -12,10 +12,7 @@ const items = ref([
     <section>
       <h3 class="text-lg font-medium mb-4">Ultimi Sondaggi</h3>
       <ul class="bg-white shadow rounded-lg divide-y">
-        <li v-for="(item, idx) in items" :key="idx" class="px-4 py-3 hover:bg-gray-50 flex justify-between">
-          <span>{{ item.title }}</span>
-          <span class="text-gray-500 text-sm">{{ item.date }}</span>
-        </li>
+        Placeholder feature futura
       </ul>
     </section>
   </template>
