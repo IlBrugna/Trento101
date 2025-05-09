be043b19800f3147eedb2e3836d0d663e769fd10
IlBrugna <davidbrugnara15@gmail.com>
2025-05-09 21:01:48 +0200

    server manda errore 400 se ID non valido

--- backend/controllers/companyController.js
+++ backend/controllers/companyController.js
@@ -1,5 +1,5 @@
 import companyModel from "../models/companyModel.js"; // IMPORTA IL MODELLO
-
+import { Types } from "mongoose"; // IMPORTA MONGOOSE
 
 // GET /company --> tutte le aziende
 export const getAllCompanies = async (req, res) => {
@@ -12,14 +12,17 @@ export const getAllCompanies = async (req, res) => {
   };
 // GET /company/:companyID --> azienda singola
 export const getSpecificCompany = async (req, res) => {
-    try {
+  try {
         const companyID = req.params.companyID; //OTTENGO ID DALLA RICHIESTA
+        if (!Types.ObjectId.isValid(companyID)) {
+           return res.status(400).json({ message: 'ID non valido' });
+        }  
         const company = await companyModel.findById(companyID); //CERCO L'AZIENDA NEL DATABASE
         if (!company) {
-            res.status(404).json({ message: 'Azienda non trovata' }); //SE NON TROVO L'AZIENDA
+            return  res.status(404).json({ message: 'Azienda non trovata' }); //SE NON TROVO L'AZIENDA
         }
-        res.status(200).json(company); //SE TROVO L'AZIENDA, RISPONDO CON I SUOI DATI
+        return res.status(200).json(company); //SE TROVO L'AZIENDA, RISPONDO CON I SUOI DATI
     } catch (error) {
-        res.status(500).json({ message: 'Errore durante il recupero dell\'azienda' }); //SE C'E' UN ERRORE
+        return res.status(500).json({ message: 'Errore durante il recupero dell\'azienda' }); //SE C'E' UN ERRORE
     }
 }
