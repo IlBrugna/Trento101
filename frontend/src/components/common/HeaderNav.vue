0cce91c490d3871b7f7c95de1346538334f95dd1
IlBrugna <davidbrugnara15@gmail.com>
2025-05-15 23:04:41 +0200

    navbar contestuale al ruolo

--- frontend/src/components/common/HeaderNav.vue
+++ frontend/src/components/common/HeaderNav.vue
@@ -28,13 +28,21 @@ async function handleLogout() {
       </div>
       <div v-if="AuthStore.isLoggedIn" class="flex items-center space-x-4">
 
-        <RouterLink to="/aziende" class="flex items-center space-x-2 text-gray-700 hover:text-blue-600">
+        <RouterLink v-if="AuthStore.role=='admin'" to="/aziende" class="flex items-center space-x-2 text-gray-700 hover:text-blue-600">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75a4.5 4.5 0 0 1-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 1 1-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 0 1 6.336-4.486l-3.276 3.276a3.004 3.004 0 0 0 2.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852Z" />
             <path stroke-linecap="round" stroke-linejoin="round" d="M4.867 19.125h.008v.008h-.008v-.008Z" />
           </svg>
-          <span >Modifica la tua pagina</span>
+          <span >Pannello di Controllo</span>
         </RouterLink>
+        <RouterLink v-else-if="AuthStore.role=='company'" to="/aziende" class="flex items-center space-x-2 text-gray-700 hover:text-blue-600">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75a4.5 4.5 0 0 1-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 1 1-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 0 1 6.336-4.486l-3.276 3.276a3.004 3.004 0 0 0 2.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852Z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.867 19.125h.008v.008h-.008v-.008Z" />
+          </svg>
+          <span >Modifica la tua Pagina</span>
+        </RouterLink>
+
 
         <button @click="handleLogout" class="flex items-center space-x-2 text-gray-700 hover:text-blue-600">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
