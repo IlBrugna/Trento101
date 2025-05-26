398cff74f7878c861f5cba322d6d8a48b7135a2f
David <davidbrugnara15@gmail.com>
2025-05-26 14:18:56 +0200

    backend get poll support

--- backend/routes/pollsRouter.js
+++ backend/routes/pollsRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import {
-    postPoll
+    postPoll,
+    getPolls
 } from '../controllers/pollsController.js'; 
 
 const router = express.Router(); //CREA ROUTER
 
 router.post('/',postPoll);
+router.get('',getPolls);
 
 export default router;
