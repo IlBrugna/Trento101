e3c641fa00e8ec1c953fe94a227b21c856462e46
Texi89 <decolalessio8903@gmail.com>
2025-05-28 10:12:52 +0200

    Revert "rotte CRUD universita"
    This reverts commit 4abd3fb6431c8041bca8b7ff8db9f21155fffb65.

--- backend/routes/serviziUniversitaRouter.js
+++ backend/routes/serviziUniversitaRouter.js
@@ -1,18 +1,8 @@
 import express from 'express';
-import {
-    getServiziUniversita,
-    getServizioUniversita,
-    postServizioUniversita,
-    putServizioUniversita,
-    deleteServizioUniversita
-} from '../controllers/serviziUniversitaController.js';
+import {getServiziUniversita} from '../controllers/serviziUniversitaController.js';
 
 const router = express.Router();
 
 router.get('/', getServiziUniversita);
-router.get('/:serviceID', getServizioUniversita);
-router.post('/', postServizioUniversita);
-router.put('/:serviceID', putServizioUniversita);
-router.delete('/:serviceID', deleteServizioUniversita);
 
 export default router;
