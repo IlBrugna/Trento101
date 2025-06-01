0683f370139f78f039d9475020aa02b6b68ac6ef
IlBrugna <davidbrugnara15@gmail.com>
2025-06-01 15:22:15 +0200

    form now accepts file

--- frontend/src/components/company/CompanyEditForm.vue
+++ frontend/src/components/company/CompanyEditForm.vue
@@ -2,7 +2,7 @@
 import { ref, watch } from 'vue';
 const emit = defineEmits(['submit']);
 const props = defineProps({ company: Object });
-
+const imageFile = ref(null);
 const form = ref({ ...props.company });
 
 watch(() => props.company, (newVal) => {
@@ -10,7 +10,26 @@ watch(() => props.company, (newVal) => {
 }, { immediate: true });
 
 const submitForm = () => {
-  emit('submit', form.value);
+  const formData = new FormData();
+
+  //TUTTI CAMPI FORM
+  for (const key in form.value) {
+    formData.append(key, form.value[key]);
+  }
+
+  //FILE SE PRESENTE
+  if (imageFile.value) {
+    formData.append('image', imageFile.value);
+  }
+
+  emit('submit', formData);
+};
+
+const handleFileUpload = (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    imageFile.value = file;
+  }
 };
 </script>
 
@@ -40,6 +59,13 @@ const submitForm = () => {
       <label class="block text-sm font-medium text-gray-700">Immagine Copertina (URL)</label>
       <input v-model="form.picture" class="border rounded w-full px-3 py-2" />
     </div>
+
+    <div>
+      <label class="block text-sm font-medium text-gray-700">Immagine Copertina (FILE)</label>
+      <input type="file" class="border rounded w-full px-3 py-2" @change="handleFileUpload"/>
+    </div>
+
+    
 
     <button type="submit"
       class="bg-blue-600 text-white font-semibold px-4 py-2 rounded hover:bg-blue-700 transition">
